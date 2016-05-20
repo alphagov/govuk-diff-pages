f038d041817c186f09069d811474dbd71b802ecf
Mo Baig <mo@mobaig.com>
2016-05-20 11:41:38 +0100

    Remove unused code and files following Visual and Html diff cleanup

--- lib/govuk/diff/pages.rb
+++ lib/govuk/diff/pages.rb
@@ -3,13 +3,6 @@ require "govuk/diff/pages/version"
 module Govuk
   module Diff
     module Pages
-      autoload :AppConfig, 'govuk/diff/pages/app_config'
-      autoload :FormatSearcher, 'govuk/diff/pages/format_searcher'
-      autoload :LinkChecker, 'govuk/diff/pages/link_checker'
-      autoload :PageIndexer, 'govuk/diff/pages/page_indexer'
-      autoload :PageSearcher, 'govuk/diff/pages/page_searcher'
-      autoload :UrlChecker, 'govuk/diff/pages/url_checker'
-
       autoload :HtmlDiff, 'govuk/diff/pages/html_diff'
       autoload :TextDiff, 'govuk/diff/pages/text_diff'
       autoload :VisualDiff, 'govuk/diff/pages/visual_diff'
@@ -22,16 +15,8 @@ module Govuk
         File.expand_path(root_dir + "/../../shots")
       end
 
-      def self.govuk_pages_file
-        config_file 'govuk_pages.yml'
-      end
-
       def self.wraith_config_template
         config_file 'wraith.yaml'
-      end
-
-      def self.settings_file
-        config_file 'settings.yml'
       end
 
       def self.config_file(filename)
--- lib/govuk/diff/pages/tasks/rakefile.rake
+++ lib/govuk/diff/pages/tasks/rakefile.rake
@@ -60,15 +60,4 @@ namespace :config do
       abort("ERROR: A required dependency is not installed")
     end
   end
-
-  desc 'update config files with list of pages to diff'
-  task :update_page_list do
-    puts "---> Updating page list"
-    Govuk::Diff::Pages::PageIndexer.new.run
-  end
-
-  desc 'checks all URLs are accessible'
-  task :check_urls do
-    Govuk::Diff::Pages::LinkChecker.new.run
-  end
 end
