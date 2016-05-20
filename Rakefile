alphagov/govuk-diff-pages

commit a8013949e5d5f686124eaa1bd13140eee52e7a4a

Remove gemspec
This project is not used as a gem yet. For easier maintenance it’s
easier to remove gem-related code until we’ve figured out the ideal
deploy/usage strategy for it.

Rakefile
@@ -1,12 +1,3 @@
 require "rspec/core/rake_task"
-require "gem_publisher"
 
 load File.dirname(__FILE__) + "/lib/govuk/diff/pages/tasks/rakefile.rake"
-
-RSpec::Core::RakeTask.new(:spec)
-task default: :spec
-
-task :publish_gem do
-  gem = GemPublisher.publish_if_updated("govuk-diff-pages.gemspec", :rubygems)
-  puts "Published #{gem}" if gem
-end

lib/govuk/diff/pages/tasks/rakefile.rake
@@ -1,5 +1,3 @@
-require 'govuk/diff/pages'
-
 namespace :diff do
   desc 'produce visual diffs - set env var `URI` with location of a yaml file containing paths to diff'
   task visual: ['config:pre_flight_check'] do

.gitignore
@@ -23,7 +23,3 @@
 
 # generated results
 /results
-
-# This is a gem
-Gemfile.lock
-*.gem

Gemfile
@@ -1,6 +1,14 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'wraith', git: 'git@github.com:alphagov/wraith.git', branch: 'psr'
-gem 'govuk-lint', '~> 0.8'
+gem "wraith", github: "alphagov/wraith", branch: "psr"
+gem "diffy", "~> 3.1"
+gem "rest-client", "~> 1.8"
+gem "nokogiri"
 
-gemspec
+gem "govuk-lint", "~> 0.8"
+
+group :development do
+  gem "rake", "~> 10.0"
+  gem "rspec", "~> 3.0"
+  gem "pry-byebug"
+end

govuk-diff-pages.gemspec
@@ -1,30 +1,0 @@
-# coding: utf-8
-lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'govuk/diff/pages/version'
-
-Gem::Specification.new do |spec|
-  spec.name          = "govuk-diff-pages"
-  spec.version       = Govuk::Diff::Pages::VERSION
-  spec.authors       = ["Ben Lovell"]
-  spec.email         = ["benjamin.lovell@gmail.com"]
-
-  spec.summary       = 'Visual and textual page diffing.'
-  spec.description   = 'Diffs web pages both visually and textually.'
-  spec.homepage      = "https://github.com/alphagov/govuk-diff-pages"
-  spec.license       = "MIT"
-  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
-
-  spec.add_dependency "diffy", "~> 3.1"
-  spec.add_dependency "rest-client", "~> 1.8"
-  spec.add_dependency "nokogiri"
-
-  spec.add_development_dependency "bundler", "~> 1.10"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
-  spec.add_development_dependency "pry-byebug"
-  spec.add_development_dependency "gem_publisher", "~> 1.1"
-end

jenkins.sh
@@ -11,8 +11,4 @@ if [[ ${GIT_BRANCH} != "origin/master" ]]; then
   bundle exec govuk-lint-ruby --format clang
 fi
 
-bundle exec rake
-
-if [[ -n "$PUBLISH_GEM" ]]; then
-  bundle exec rake publish_gem
-fi
+bundle exec rspec spec

lib/govuk/diff/pages.rb
@@ -1,5 +1,3 @@
-require "govuk/diff/pages/version"
-
 module Govuk
   module Diff
     module Pages

lib/govuk/diff/pages/version.rb
@@ -1,7 +1,0 @@
-module Govuk
-  module Diff
-    module Pages
-      VERSION = "0.1.1"
-    end
-  end
-end

commit d110f402c72491d10fbe8f10e96f33fdaaa890b4

Fix loading of files in lib when executing rake tasks
This broke in a8013949e.

Rakefile
@@ -1,3 +1,5 @@
 require "rspec/core/rake_task"
 
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 load File.dirname(__FILE__) + "/lib/govuk/diff/pages/tasks/rakefile.rake"

lib/govuk/diff/pages/tasks/rakefile.rake
@@ -1,3 +1,5 @@
+require "govuk/diff/pages"
+
 namespace :diff do
   desc 'produce visual diffs - set env var `URI` with location of a yaml file containing paths to diff'
   task visual: ['config:pre_flight_check'] do